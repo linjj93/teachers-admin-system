935062b28bde41d99999cc757bdbce33d0b64c2d
linjj93 <linjj93@hotmail.com>
2019-09-02 09:52:32 +0800

    Initial commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("app", () => {
+  it("should test that jest is working", () => {
+    expect(1).toBe(1);
+  });
+});
